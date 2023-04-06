d7d4b4f1687dc3a6cba9115697b4bb0dd49a744f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-05 22:08:17 -0300

    Feat: Usando os reducers

--- state-management/src/store/index.ts
+++ state-management/src/store/index.ts
@@ -1,7 +1,8 @@
 // Aqui vai ficar todo o codigo de gerenciamento de estado
 
 import { createStore } from "redux";
+import rootReducer from "./modules/rootReducer";
 
-const store = createStore(() => []);
+const store = createStore(rootReducer);
 
 export default store;
