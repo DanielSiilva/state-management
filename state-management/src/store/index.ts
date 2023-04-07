50c81e09d9e20c5132c2621163b299bb8b751d9c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-07 11:38:48 -0300

    Feat: Debugando com DevTools

--- state-management/src/store/index.ts
+++ state-management/src/store/index.ts
@@ -2,6 +2,7 @@
 
 import { createStore } from "redux";
 import rootReducer from "./modules/rootReducer";
+import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
 
 import { ICartState } from "./modules/cart/types";
 
@@ -10,6 +11,9 @@ export interface IState {
 }
 
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+);
 
 export default store;
