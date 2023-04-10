2230ee495e9e5fa9338366a10ff8148a37ae2351
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-10 00:24:28 -0300

    Feat: Configurando Redux Saga - padrão

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,33 @@
 // Aqui vai ficar todo o codigo de gerenciamento de estado
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from 'redux-saga'
 
 import { ICartState } from "./modules/cart/types";
+import rootSaga from "./modules/rootSaga";
 
 export interface IState {
     cart: ICartState
 }
 
+const sagaMiddleware = createSagaMiddleware()
+
+const middleware = [sagaMiddleware]
+
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(
+            ...middleware
+        )
+    )
 );
 
 //composeWithDevTools  p/ usarmos a extensão no navegador e analizar as ações do redux
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
